15e68e26e43c7126b8def2dcab4ae0fc292e04c0
mchu-1 <mchu@gmail.com>
2023-08-07 10:38:07 +1000

    Update index.jl

--- src/index.jl
+++ src/index.jl
@@ -1,35 +1,12 @@
 # index.jl
 
-module index
-using StatsBase
-
 """
 Index diversity of barcode sequencing data.
 """
 
-struct TrimmingError <: Exception
-    msg::String
-end
+module index
 
-function generate_windows(file::String, k::Int)
-    """
-    Generate matrix of k-mer sequence windows from file.
-    """
-    # Read in sequences
-    V = readlines(file)
-    V = filter(!isempty, V)
-    if length(Set([length(v) for v in V])) != 1
-        throw(TrimmingError("Sequences have not been trimmed to equal length."))
-    end
-
-    # Generate windows matrix
-    m = length(v[1])
-    n = length(V)-k+1
-    A = [V[i][j:j+k] for i in 1:n for j in 1:m]
-
-    return A
-end
-    
+using StatsBase
 
 function get_dist(a::Vector{String})
     """
@@ -42,7 +19,7 @@ function get_dist(a::Vector{String})
 end
 
 
-function index_diversity(A::Matrix{String}, q::Float64)
+function index_diversity(A::Matrix{String}, q::AbstractFloat)
     """
     Index diversity of sequence windows.
     """
@@ -52,7 +29,6 @@ function index_diversity(A::Matrix{String}, q::Float64)
     return W
 end
 
-export generate_windows
 export get_dist
 export index_diversity
 
