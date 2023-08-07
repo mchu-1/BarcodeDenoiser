8ff2e9e9bd18bfc0bb1fe893ce7c814514ff7179
mchu-1 <mchu@gmail.com>
2023-08-07 19:45:08 +1000

    Update test_trim.jl

--- test/test_trim.jl
+++ test/test_trim.jl
@@ -27,6 +27,14 @@ using Test
 end
 
 
+@testset "Trim sequence" begin
+    seq = "NNNACTGMMM"
+    trimmed_seq = "ACTG"
+    @test trim.trim_sequence(seq, "NNN", "MMM") == trimmed_seq
+    @test trim.trim_sequence(seq, "PPP", "MMM") ===  nothing
+end
+
+
 @testset "Generate sequence windows" begin
     V = ["AAAAT", "GGGGC"]
     @test trim.generate_windows(V, 3) == ["AAA" "AAA" "AAT"; "GGG" "GGG" "GGC"]
